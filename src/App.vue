02064c4743b5fb235ffde809106573a223c376a5
aadil-dev <adil62742@gmail.com>
2019-09-18 15:00:20 +0500

    Forms inputs with vue.js

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <app-form></app-form>
+</template>
+
+<script>
+import Form from './Form.vue';
+
+export default {
+    components:{
+        'app-form': Form
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
